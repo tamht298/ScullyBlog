c0d7731804693a4fd048468726c1e1047a5a03d1
Thanh Tam <tamht298@gmail.com>
2020-06-11 21:36:27 +0700

    generate home module

--- scully.ng-scully.config.ts
+++ scully.ng-scully.config.ts
@@ -4,5 +4,11 @@ export const config: ScullyConfig = {
   projectName: "ng-scully",
   outDir: './dist/static',
   routes: {
+    '/blog/:slug': {
+      type: 'contentFolder',
+      slug: {
+        folder: "./blog"
+      }
+    },
   }
 };
